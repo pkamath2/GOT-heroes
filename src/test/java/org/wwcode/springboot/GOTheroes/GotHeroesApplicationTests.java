1aca1e135e1d655f6826d6d15ea0d09a534f0625
pkamath2 <git.pkamath2@gmail.com>
2018-01-07 17:48:38 +0800

    Starter commit

---
+++ src/test/java/org/wwcode/springboot/GOTheroes/GotHeroesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wwcode.springboot.GOTheroes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GotHeroesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
